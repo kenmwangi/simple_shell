1298fef948379f820a99452c6825f7b7b154312f
Ken Mwangi <kenmwangi071@gmail.com>
2022-11-14 21:27:07 -0800

    prototypes functions

--- shell.h
+++ shell.h
@@ -117,6 +117,10 @@ int find_builtin(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
 
+/* buildin.c */
+void my_exit(shell_t *);
+void print_env(shell_t *);
+
 /* path.c */
 int is_cmd(info_t *, char *);
 char *dup_chars(char *, int, int);
